ff0fae00b462e4c5b37cbbb15c785eb8260bc815
adrian <bella.adus@gmail.com>
2018-03-28 16:51:58 +0200

    MVP v1 with logging and saveweights function

--- agent.py
+++ agent.py
@@ -5,42 +5,48 @@ from model import DQNModel
 
 
 class DQNAgent:
-    def __init__(self, env):
+    def __init__(self, env, action_size):
         self.memory = deque(maxlen=2000)
         self.gamma = 0.95    # discount rate
         self.epsilon = 0.9  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
+        self.action_size = action_size
         self.env = env
         self.dqn_model = DQNModel(self.learning_rate)
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+    def remember(self, state, action, reward, next_state):
+        self.memory.append((state, action, reward, next_state))
 
     def action(self, fi_t):
 
         num_random = random.uniform(0, 1)
 
-        if num_random <= self.epsilon:
-            return self.env.action_space.sample()
+        if num_random <= self.epsilon:  # with probability epsilon do a random action
+            return random.randrange(self.action_size)
+        else:
+            fi_t = np.expand_dims(fi_t, axis=0)
+            action = self.dqn_model.model.predict(fi_t)
+            return np.argmax(action[0])
 
-        action = self.dqn_model.model.predict(fi_t)
+    def replay(self, batch_size, done):
 
-        return np.argmax(action[0])
+        mini_batch = random.sample(self.memory, batch_size)  # sample random mini_batch from D
 
-    def replay(self, batch_size):  #TODO: replace target and simple model to the right places
+        for state, action, reward, next_state in mini_batch:
 
-        mini_batch = random.sample(self.memory, batch_size)
+            next_state = np.expand_dims(next_state, axis=0)
+            state = np.expand_dims(state, axis=0)
 
-        for state, action, reward, next_state, done in mini_batch:
-            target = reward
+            if done:
+                target = reward
+            else:
+                target = (reward + self.gamma * np.amax(self.dqn_model.target_model.predict(next_state)[0]))
 
-            if not done:
-                target = (reward + self.gamma * np.amax(self.dqn_model.model.Sequential.predict(next_state)[0]))
-
-            target_f = self.dqn_model.model.Sequential.predict(state)
+            target_f = self.dqn_model.model.predict(state)
             target_f[0][action] = target
+            #Trains the model for a fixed number of epochs (iterations on a dataset)
             self.dqn_model.model.fit(state, target_f, epochs=1, verbose=0)
 
         if self.epsilon > self.epsilon_min:
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import gym as gym
 import numpy as np
+import datetime
+import logging
 
 from agent import DQNAgent
 from state_builder import StateBuilder
@@ -8,23 +10,24 @@ EPISODES = 1000
 
 if __name__ == "__main__":
     env = gym.make('Breakout-v0')
-    agent = DQNAgent(env)
+    action_size = env.action_space.n
+    agent = DQNAgent(env, action_size)
     done = False
     batch_size = 32
+    logging.basicConfig(filename='./log/'+str(datetime.datetime.now())+'.log', level=logging.DEBUG)
     # agent.load("./save/breakout.h5")
-    #TODO:initialize action-value function with random weights teta
-    #TODO:initialize target action-value function with weights teta- = teta
 
     for i_episode in range(EPISODES):
         CNN_input_stack = np.zeros((5, 84, 84))  # initialization of the CNN input
         observation = env.reset()  # s1 = {x1}
         CNN_input_stack[0] = StateBuilder.pre_process(observation)
         action = env.action_space.sample()
+        done = False
+        sum_reward = 0
 
-        for t in range(500):
+        for t in range(5000):
 
             env.render()
-
 
             if t >= 4:
 
@@ -32,6 +35,8 @@ if __name__ == "__main__":
 
                 action = agent.action(fi_t)
                 observation, reward, done, info = env.step(action)
+
+                sum_reward += reward
 
                 CNN_input_stack[0] = CNN_input_stack[1]  # overlap
                 CNN_input_stack[1] = CNN_input_stack[2]
@@ -41,19 +46,24 @@ if __name__ == "__main__":
 
                 fi_t1 = CNN_input_stack[1:5]  # fi_t+1 = fi(s_t+1)
 
-                agent.remember(fi_t, action, reward, fi_t1,done)
+                agent.remember(fi_t, action, reward, fi_t1)  # Store transition (fi_t,a_t,r_t,fi_t+1) in D
 
             else:
                 CNN_input_stack[(t + 1)] = StateBuilder.pre_process(observation)
 
             if done:
-                print("Episode finished after {} timesteps".format(t + 1))
-                print(info)
+                logging.info("episode: {}/{}, score: {}, epsilone: {:.2}, timestep: {}"
+                      .format(i_episode, EPISODES, sum_reward, agent.epsilon, t))
+                print("episode: {}/{}, score: {}, epsilone: {:.2}, timestep: {}"
+                      .format(i_episode, EPISODES, sum_reward, agent.epsilon, t))
                 break
 
-        if len(agent.memory) > batch_size:
-            agent.replay(batch_size)
+        agent.dqn_model.update_target_model()  # update target model every C step
 
-        if i_episode % 10 == 0:
-           agent.save("./save/breakout.h5")
+        if len(agent.memory) > batch_size:
+            agent.replay(batch_size, done)
+
+        if i_episode % 100 == 0:
+            filename = "./save/breakout_"+str(datetime.datetime.now())+".h5"
+            agent.save(filename)
 
